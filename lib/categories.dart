ca98487faca6b79bbd1af6b8bff54981c862f44b
scott kelly <scott.kelly@crtr.co.uk>
2023-08-07 10:52:04 +0100

    create categories view

---
+++ lib/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(
+        title: const Text('Pick a Category'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Colors.amber,
+            child: Center(child: Text('$index')),
+          );
+        }
+      ),
+    );
+  }
+}
