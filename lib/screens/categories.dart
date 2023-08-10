d3d8d9dbdd54d8f2a607a374dd1a06c198e9be91
scott kelly <scott.kelly@crtr.co.uk>
2023-08-10 17:10:53 +0100

    adds meal items on meals screen when category is clicked

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
+
+//screens
+import 'package:flutter_meal_app/screens/meals.dart';
+
+//widgets
 import 'package:flutter_meal_app/widgets/category_grid_item.dart';
 
+//models
+import 'package:flutter_meal_app/models/category.dart';
+
+//data
 import 'package:flutter_meal_app/data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: category.title, meals: filteredMeals)));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Pick a Category'),
       ),
       body: GridView.builder(
-        padding: const EdgeInsets.all(24),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        itemCount: availableCategories.length,
-        itemBuilder: (BuildContext context, int index) {
-          final category = availableCategories[index];
-          return CategoryGridItem(category: category);
-        }
-      ),
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          itemCount: availableCategories.length,
+          itemBuilder: (BuildContext context, int index) {
+            final category = availableCategories[index];
+            return CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                });
+          }),
     );
   }
 }
