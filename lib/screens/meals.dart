875072c1c59fbcd2e765ff13ba38ebdc05ba6da2
scott kelly <scott.kelly@crtr.co.uk>
2023-08-10 14:46:02 +0100

    adds screen an model for meals

---
+++ lib/screens/meals.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_meal_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) => Text(
+            meals[index].title,
+          ),
+        ));
+  }
+}
