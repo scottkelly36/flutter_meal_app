cebf4bae16e3ba2fe2f41790bc5c30841b437b73
scott kelly <scott.kelly@crtr.co.uk>
2023-08-11 14:26:46 +0100

    improves recipe screen to include ingredients and steps

--- lib/screens/recipe.dart
+++ lib/screens/recipe.dart
@@ -1,26 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meal_app/models/meal.dart';
 
-class RecipeScreen extends StatelessWidget{
-  const RecipeScreen({
-    super.key,
-    required this.meal
-  });
+class RecipeScreen extends StatelessWidget {
+  const RecipeScreen({super.key, required this.meal});
 
   final Meal meal;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
-        )
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 14),
+              Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              for (final ingredient in meal.ingredients)
+                Text(ingredient,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        )),
+              const SizedBox(height: 24),
+              Text(
+                'Steps',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 14),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: meal.steps.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final step = entry.value;
+        
+                  return Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    child: Text(
+                      '${index + 1}. $step',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ), textAlign: TextAlign.left,
+                    ),
+                  );
+                }).toList(),
+              )
+            ],
+          ),
+        ));
   }
 }
