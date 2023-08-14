622f28c576470d338d4ba0211ce33dae1a8ff3d2
scott kelly <scott.kelly@crtr.co.uk>
2023-08-14 14:06:52 +0100

    starts to add screen switching to Drawer

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({Key? key}) : super(key: key);
+  const MainDrawer({
+    super.key,
+    required this.onSelectScreen
+});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +55,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+              onSelectScreen('meals');
+            },
           ),
           ListTile(
             leading: Icon(
@@ -65,7 +72,9 @@ class MainDrawer extends StatelessWidget {
                     fontSize: 24,
                   ),
             ),
-            onTap: () {},
+            onTap: () {
+               onSelectScreen('filters');
+            },
           ),
         ],
       ),
