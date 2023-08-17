0bde3dcc1bba33bc59b4dcfb0a98144f2e6a2ee6
scott kelly <scott.kelly@crtr.co.uk>
2023-08-17 12:32:07 +0100

    starts to move some state to riverpod

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:flutter_meal_app/screens/categories.dart';
 import 'package:flutter_meal_app/screens/filters.dart';
 import 'package:flutter_meal_app/screens/meals.dart';
 import 'package:flutter_meal_app/models/meal.dart';
 import 'package:flutter_meal_app/widgets/main_drawer.dart';
+import 'package:flutter_meal_app/providers/meals_provider.dart';
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
   _TabsScreenState createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
@@ -68,7 +70,9 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final availableMeals = dummyMeals.where((meal) {
+    final meals = ref.watch(mealsProvider);
+
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree){
         return false;
       }
